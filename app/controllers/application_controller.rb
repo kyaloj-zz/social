c8711bed57922856c162dbf77415b34c168a1dd0
Justus <mj.kyalo@gmail.com>
2017-09-15 00:13:58 +0300

    authenticating user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 #
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   private
 
@@ -9,4 +10,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id])
   end
   helper_method :current_user
+
+  def authenticate_user!
+    redirect_to login_path unless current_user
+  end
 end
