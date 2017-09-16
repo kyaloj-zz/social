da0784b1b4447910f60ffb33653f831b2a752ecc
Justus <mj.kyalo@gmail.com>
2017-09-16 18:16:11 +0300

    security updates

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :sessions
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
   root to: 'posts#index'
 end
