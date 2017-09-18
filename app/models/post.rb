3c1c7bc839db09d65888cb1cce0e25d6296f9040
Justus <mj.kyalo@gmail.com>
2017-09-18 19:11:31 +0300

    turbolinks, caching, eager loads and all performance improvement thingys

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many   :comments
   validates :user_id, :type, presence: true
+
+  def cached_comment_count
+    Rails.cache.fetch [self, 'comment_count'] do
+      comments.size
+    end
+  end
 end
