485b0e0748feee2884dd3f0ad4ae1efe9435b45a
Justus <mj.kyalo@gmail.com>
2017-09-19 21:21:10 +0300

    authentication set ups

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
   has_many :image_posts, dependent: :destroy
   has_many :comments
 
+  validates :api_token, presence: true, uniqueness: true
+  before_validation :generate_api_token
+
   def following?(leader)
     leaders.include? leader
   end
@@ -24,5 +27,13 @@ class User < ApplicationRecord
 
   def timeline_user_ids
     leader_ids + [id]
+  end
+
+  def generate_api_token
+    return if api_token.present?
+    loop do
+      self.api_token = SecureRandom.hex
+      break unless User.exists? api_token: api_token
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914173551) do
+ActiveRecord::Schema.define(version: 20170919175722) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170914173551) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "api_token"
   end
 
 end
