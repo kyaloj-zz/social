2f2e61eec5dc4a3f486c85db56950cb603f7444b
Justus <mj.kyalo@gmail.com>
2017-09-19 21:27:08 +0300

    set up the authentication process

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,13 @@
+#
 class ApiController < ApplicationController
   skip_before_filter :authenticate_user!
+  before_filter :authenticate_token!
+
+  private
+
+  def authenticate_token!
+    authenticate_or_request_with_http_token do |token, _options|
+      @api_user = User.find_by(api_token: token)
+    end
+  end
 end
